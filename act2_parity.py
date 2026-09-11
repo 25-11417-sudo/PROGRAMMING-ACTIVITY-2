0a14291302b300909234f8c7d439024e2f6c9eaa
25-11417-sudo <25-11417@g.batstate-u.edu.ph>
2026-09-11 12:04:49 +0800

    Update act2_parity.py

--- act2_parity.py
+++ act2_parity.py
@@ -1,21 +1,7 @@
-# Name: Rodriguez jeremie G.
-# Section: BAET - 2101
-# Task 1 - Change Calculator
+# Name: RODRIGUEZ JEREMIE.
+# Section: BAET 2101
+# Task 2 - Even or Odd
 
-amount = int(input("Enter amount in pesos: "))
+number = int(input("Enter an integer: "))
 
-pesos100 = amount // 100
-amount = amount % 100
-
-pesos20 = amount // 20
-amount = amount % 20
-
-pesos5 = amount // 5
-amount = amount % 5
-
-pesos1 = amount // 1
-
-print("100 pesos:", pesos100)
-print("20 pesos:", pesos20)
-print("5 pesos:", pesos5)
-print("1 peso:", pesos1)
+print(number % 2 == 0)
